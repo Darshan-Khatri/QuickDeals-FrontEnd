5d101ed15663089775ffa8019787f5ba6d7d8085
Darshan-Khatri <darshankhatri2297@gmail.com>
2021-01-18 01:17:54 -0600

    Adding toastr module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
@@ -8,10 +9,14 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
   ],
 
   exports: [
     BsDropdownModule,
+    ToastrModule,
   ]
 })
 export class SharedModule { }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../Service/account.service';
@@ -16,6 +17,7 @@ export class NavComponent implements OnInit {
   constructor(
     public accountService: AccountService,
     private router: Router,
+    private toastr: ToastrService,
     ) { }
 
   ngOnInit(): void {}
@@ -24,13 +26,20 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response => {
       //this.accountService.currentUser$.subscribe(user => console.log(user));
+      this.toastr.success('Success');
       this.router.navigateByUrl('/frontPage');
+    },
+    (err) => {
+      console.log(err.error);
+      this.toastr.error(err.error);
     });
   }
 
 
   logout() {
     this.accountService.logout();
+    this.toastr.show('Logged Out');
+    this.router.navigateByUrl('/');
   }
 
 }
