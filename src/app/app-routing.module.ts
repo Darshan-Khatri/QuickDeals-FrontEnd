1d49c5c56d6cbbc622bac5d95c5846b26e4861bb
Darshan-Khatri <darshankhatri2297@gmail.com>
2021-01-18 14:25:04 -0600

    Auth guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { ForumComponent } from './Deals/forum/forum.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'frontPage', component: FrontPageComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'frontPage', component: FrontPageComponent },
+      { path: 'forumPage', component: ForumComponent },
+    ]
+  },
   { path: 'register', component: RegisterComponent },
-  { path: 'forumPage', component: ForumComponent },
 ];
 
 @NgModule({
