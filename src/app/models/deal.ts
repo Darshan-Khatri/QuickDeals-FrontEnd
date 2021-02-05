2b8c385dd373e17b9872eaa5e86f5cbf61d63726
Darshan-Khatri <darshankhatri2297@gmail.com>
2021-02-05 17:09:52 -0600

    Now deal model includes comments collection too

--- src/app/models/deal.ts
+++ src/app/models/deal.ts
@@ -2,7 +2,7 @@
 /*Be careful while writing data types because server sends data in camelCase so if you don't write your properties in camelCase then
   you will get response back from server but our angular page won't load that data because it does not find properties in camelCase here*/
 export interface Deal {
-  id : number;
+  id: number;
   title: string;
   content: string;
   url: string;
@@ -13,9 +13,18 @@ export interface Deal {
   likes: number;
   disLikes: number;
   photos: Photo[];
+  comments: Comment[];
 }
 
 interface Photo {
   id: number;
   url: string;
+}
+
+
+interface Comment {
+  username: string
+  commentText: string
+  commentDate: Date
+  dealId: number
 }
